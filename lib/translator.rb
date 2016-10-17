8027586851a9d0b22d400cc0c4b1b701173eae10
Jason Conrad <jdconrad@liberty.edu>
2016-10-17 16:58:02 -0600

    updated translation that includes numbers

--- lib/translator.rb
+++ lib/translator.rb
@@ -21,38 +21,43 @@ class Translator
     @top_line = []
     @middle_line = []
     @bottom_line = []
-    @decrypted = []
+    # @decrypted = []
   end
 
-  def prepare_the_message_for_translation(message)
+  def prepare_the_message_for_translation_to_braille(message)
     @prepared = message.chars.flatten
   end
 
   def translates_top_line_from_english_to_braille(message)
-    prepare_the_message_for_translation(message)
+    prepare_the_message_for_translation_to_braille(message)
     @encrypted_top = @prepared.map do |letter|
       alphabet_1[letter]
     end.flatten
   end
 
   def translates_middle_line_from_english_to_braille(message)
-    prepare_the_message_for_translation(message)
+    prepare_the_message_for_translation_to_braille(message)
     @encrypted_middle = @prepared.map do |letter|
       alphabet_2[letter]
     end.flatten
   end
 
   def translates_bottom_line_from_english_to_braille(message)
-    prepare_the_message_for_translation(message)
+    prepare_the_message_for_translation_to_braille(message)
     @encrypted_bottom = @prepared.map do |letter|
       alphabet_3[letter]
     end.flatten
   end
 
+  def prepare_message_for_translation_to_english(top_line, middle_line, bottom_line)
+    @encrypted = top_line.zip(middle_line, bottom_line)
+  end
 
-
-  def translate_top_line_from_braille_to_english(encrypted)
-    encrypted.map do |symbol|
+  def translates_from_braille_to_english(top_line, middle_line, bottom_line)
+    # binding.pry
+    prepare_message_for_translation_to_english(top_line, middle_line, bottom_line)
+    decrypted = []
+    @encrypted.map do |symbol|
       alphabet_4.has_value?(symbol)
       if true
       decrypted  << alphabet_4.key(symbol)
