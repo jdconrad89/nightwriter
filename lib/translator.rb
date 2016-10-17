b7f9261b23f0aaa32e4610d02ad2673956c44344
Jason Conrad <jdconrad@liberty.edu>
2016-10-17 10:49:33 -0600

    finished updated translator with 100% test coverage

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,28 +4,59 @@ require 'pry'
 class Translator
   include Dictionary
 
+  attr_reader :alphabet_1,
+              :alphabet_2,
+              :alphabet_3,
+              :alphabet_4,
+              :encrypted_top,
+              :encrypted_middle,
+              :encrypted_bottom,
+              :decrypted
+
   def initialize
-    @dictionary = Dictionary.dictionary
+    @alphabet_1 = Dictionary.top_line
+    @alphabet_2 = Dictionary.middle_line
+    @alphabet_3 = Dictionary.bottom_line
+    @alphabet_4 = Dictionary.decryption_dictionary
+    @top_line = []
+    @middle_line = []
+    @bottom_line = []
+    @decrypted = []
   end
 
   def prepare_the_message_for_translation(message)
-    prepared = message.chars.flatten
+    @prepared = message.chars.flatten
   end
 
-  def translates_from_english_to_braille(message)
+  def translates_top_line_from_english_to_braille(message)
     prepare_the_message_for_translation(message)
-    @encrypted = @prepared.map do |letter|
-      @dictionary.values_at(letter)
+    @encrypted_top = @prepared.map do |letter|
+      alphabet_1[letter]
     end.flatten
   end
 
-  def translate_from_braille_to_english(encrypted)
-    encrypted.map do |symbol|
-      @dictionary.has_value?(symbol)
-      if true
-        @dictionary.key(symbol)
-      end
-    end.join
+  def translates_middle_line_from_english_to_braille(message)
+    prepare_the_message_for_translation(message)
+    @encrypted_middle = @prepared.map do |letter|
+      alphabet_2[letter]
+    end.flatten
   end
 
+  def translates_bottom_line_from_english_to_braille(message)
+    prepare_the_message_for_translation(message)
+    @encrypted_bottom = @prepared.map do |letter|
+      alphabet_3[letter]
+    end.flatten
+  end
+
+
+
+  def translate_top_line_from_braille_to_english(encrypted)
+    encrypted.map do |symbol|
+      alphabet_4.has_value?(symbol)
+      if true
+      decrypted  << alphabet_4.key(symbol)
+    end
+  end.uniq.join
+  end
 end
--- test/translator_test.rb
+++ test/translator_test.rb
@@ -15,15 +15,18 @@ class TranlatorTest < Minitest::Test
   def test_it_translates_english_into_braille
     t = Translator.new
 
-    result = t.translates_from_english_to_braille("This is the message")
-
-    assert_equal [".....0.0000.", "0.00..", ".00...", ".00.0.", "......", ".00...", ".00.0.", "......", ".0000.", "0.00..", "0..0..", "......", "00..0.", "0..0..", ".00.0.", ".00.0.", "0.....", "0000..", "0..0.."], result
+    result_1 = t.translates_top_line_from_english_to_braille("This is the message")
+    result_2 = t.translates_middle_line_from_english_to_braille("This is the message")
+    result_3 = t.translates_bottom_line_from_english_to_braille("This is the message")
+    assert_equal ["...0", "0.", ".0", ".0", "..", ".0", ".0", "..", ".0", "0.", "0.", "..", "00", "0.", ".0", ".0", "0.", "00", "0."], result_1
+    assert_equal ["..00", "00", "0.", ".0", "..", "0.", ".0", "..", "00", "00", ".0", "..", "..", ".0", ".0", ".0", "..", "00", ".0"], result_2
+    assert_equal ["...0", "..", "..", ".0", "..", "..", ".0", "..", ".0", "..", "..", "..", "0.", "..", ".0", ".0", "..", "..", ".."], result_3
   end
 
   def test_it_translates_braille_into_english
     t = Translator.new
 
-    result = t.translate_from_braille_to_english([".....0.0000.", "0.00..", ".00...", ".00.0.", "......", ".00...", ".00.0.", "......", ".0000.", "0.00..", "0..0..", "......", "00..0.", "0..0..", ".00.0.", ".00.0.", "0.....", "0000..", "0..0.."])
+    result = t.translate_top_line_from_braille_to_english([["....", ".0.0", "000."], ["0." , "00", ".."], [".0", "0.", ".."], [".0", "0.", "0."], ["..", "..", ".."], [".0", "0.", ".."], [".0", "0.", "0."], ["..", "..", ".."], [".0", "00","0."], ["0." , "00", ".."], ["0.", ".0", ".."], ["..", "..", ".."], ["00", "..", "0."], ["0.", ".0", ".."], [".0", "0.", "0."], [".0", "0.", "0."], ["0.", "..", ".."], ["00", "00", ".."], ["0.", ".0", ".."]])
 
     assert_equal "This is the message", result
   end
